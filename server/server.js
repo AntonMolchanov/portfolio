ba45e7f3a11fb1f29e1c71070f703f1871e29637
AntonMolchanov <antonmolchanov97@gmail.com>
2021-10-02 14:59:24 +0300

    accomplished portfolio

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 require ('dotenv').config()
+console.log(process.env)
 const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, '../build')));
@@ -10,4 +11,5 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-app.listen(port);
+app.listen(port,() => {
+    console.log(`server started on port ${port}`)});
