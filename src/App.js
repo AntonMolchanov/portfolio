ed91c653089ff73380e3b0fc873dc42fc190be76
AntonMolchanov <antonmolchanov97@gmail.com>
2021-10-02 13:25:17 +0300

    accomplished portfolio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import Header from "./components/Header/Header";
 import Portfolio from "./pages/Portfolio/Portfolio";
-import PortfolioNew from "./pages/PortfolioNew/Portfolio";
 
 function App() {
 
